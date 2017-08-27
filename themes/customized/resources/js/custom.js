917dfc28eb786a323d27756d22b7b669ed8257a0
J. Zhao <junzhouzhao@gmail.com>
2017-08-27 17:00:12 +0800

    rm custom.js

--- themes/customized/resources/js/custom.js
+++
@@ -1,6 +0,0 @@
-
-$(document).ready(function() {
-    $(window).scroll(function(){
-      $("#rightmenu").stop().animate({"marginTop": ($(window).scrollTop()) + "px", "marginLeft":($(window).scrollLeft()) + "px"}, "fast" );
-    });
-});
